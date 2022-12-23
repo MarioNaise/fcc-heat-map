aa9da2f35e45a5f2305ce5312cc7f36e76417cb1
Edwin Harmuth <102912402+MarioNaise@users.noreply.github.com>
2022-12-23 14:51:50 +0000

    added x axis

--- script.js
+++ script.js
@@ -8,14 +8,20 @@
   }
 
   const renderData = async () => {
-    const dataset = await getData();
+    const data = await getData();
+    const dataset = data.monthlyVariance;
+    const { baseTemperature } = data;
+
     const w = 800;
     const h = 500;
     const padding = 50;
 
+    const minYear = d3.min(dataset, (d)=>d.year);
+    const maxYear = d3.max(dataset, (d)=>d.year);
+
     // SET DESCRIPTION
     d3.select("#description")
-    .text(`${d3.min(dataset.monthlyVariance, (d)=>d.year)} - ${d3.max(dataset.monthlyVariance, (d)=>d.year)}: base temperature ${dataset.baseTemperature}°C`);
+    .text(`${minYear} - ${maxYear}: base temperature ${baseTemperature}°C`);
 
     // APPEND SVG
     const svg = d3.select("main")
@@ -23,6 +29,21 @@
       .attr("width", w)
       .attr("height", h)
 
+    // SET SCALES
+    const xScale = d3.scaleTime()
+      .domain([new Date(minYear.toString()), new Date(maxYear.toString())])
+      .range([padding, w - padding]);
+
+    // APPEND AXES
+    const xAxis = d3.axisBottom(xScale);
+    
+    svg.append("g")
+       .attr("transform", "translate(0," + (h - padding) + ")")
+       .call(xAxis)
+       .attr("id", "x-axis");
+
+
+    
   }
 
   renderData();
