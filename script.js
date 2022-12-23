576a1d2729fa658a663e6d5151667306e9b791e3
Edwin Harmuth <102912402+MarioNaise@users.noreply.github.com>
2022-12-23 22:37:48 +0000

    fix colors and position of cells (still wip)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function(){
   const dataUrl = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
-  colors = ["#ef5350","#EC407A","#AB47BC","#7E57C2","#5C6BC0","#42A5F5","#26C6DA","#26A69A","#D4E157","#FFEE58","#FFA726"],
+ colors = ["#4575B4", "#74ADD1", "#ABD9E", "#E0F3F8", "#FFFFBF", "#FEE090", "#FDAE61", "#F46D43", "#D73027"],
   months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 
   const getData = async () => {
@@ -14,13 +14,18 @@
     const data = await getData();
     const dataset = data.monthlyVariance;
     const { baseTemperature } = data;
+    dataset.forEach((d)=>{
+      d.month = d.month-1;
+    })
 
-    const w = 800;
-    const h = 400;
+    const w = 1500;
+    const h = 700;
     const padding = 80;
 
     const minYear = d3.min(dataset, (d)=>d.year);
     const maxYear = d3.max(dataset, (d)=>d.year);
+    const minMonth = d3.min(dataset, (d)=>d.month);
+    const maxMonth = d3.max(dataset, (d)=>d.month);
 
     // SET DESCRIPTION
     d3.select("#description")
@@ -35,21 +40,16 @@
 
     // SET SCALES
     const xScale = d3.scaleTime()
-      .domain([new Date(minYear-5, 1), new Date(maxYear+5, 1)])
+      .domain([minYear, maxYear])
       .range([padding, w - padding])
 
-    let monthsDateObj = [];
-    for(let i = 0; i < 12; i++){
-      monthsDateObj.push(new Date(1970, i))
-    }
-
-    const yScale = d3.scaleBand()
-      .domain(monthsDateObj)
+    const yScale = d3.scaleLinear()
+      .domain([minMonth, maxMonth])
       .range([padding, h - padding])
 
     const colorScale = d3.scaleLinear()
       .domain(d3.extent(dataset,(d)=>d.variance))
-      .range(["yellow", "red"])
+      .range(["white", "red"])
 
     
     // APPEND AXES
@@ -83,11 +83,13 @@
     .attr("data-month", (d) => d.month)
     .attr("data-year", (d) => d.year)
     .attr("data-temp", baseTemperature)
-    .attr("x", (d,i) => xScale(d.year+i))
-    .attr("y", d => yScale(d.month))
-    .attr("width", 5)
-    .attr("height", d => yScale.bandwidth())
-    .attr("fill", (d => colorScale(d.variance)))
+    .attr("x", d =>{
+      return xScale(d.year)-padding;
+    })
+    .attr("y", d => yScale(d.month)-padding-50)
+    .attr("width", "10px")
+    .attr("height", "45px")
+    .attr("fill", d => colorScale(d.variance))
 
     
   }
