9bc9544a755ca20be711b020023192c56d1d356d
Edwin Harmuth <102912402+MarioNaise@users.noreply.github.com>
2022-12-23 16:02:46 +0000

    added y axis (WIP)

--- script.js
+++ script.js
@@ -12,12 +12,14 @@
     const dataset = data.monthlyVariance;
     const { baseTemperature } = data;
 
-    const w = 800;
-    const h = 500;
-    const padding = 50;
+    const w = 1100;
+    const h = 600;
+    const padding = 80;
 
     const minYear = d3.min(dataset, (d)=>d.year);
     const maxYear = d3.max(dataset, (d)=>d.year);
+    const minMonth = d3.min(dataset, (d)=>d.month);
+    const maxMonth = d3.max(dataset, (d)=>d.month);
 
     // SET DESCRIPTION
     d3.select("#description")
@@ -34,14 +36,26 @@
       .domain([new Date(minYear.toString()), new Date(maxYear.toString())])
       .range([padding, w - padding]);
 
+    const yScale = d3.scaleTime()
+      .domain([new Date("2002-01-01"), new Date("2002-12-31")])
+      .range([padding, h - padding]);
+
     // APPEND AXES
     const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale)
+      .ticks(12)
+      .tickFormat(d3.timeFormat("%B"))
+    
     
     svg.append("g")
        .attr("transform", "translate(0," + (h - padding) + ")")
        .call(xAxis)
        .attr("id", "x-axis");
-
+    
+    svg.append("g")
+       .attr("transform", "translate(" + padding + ",0)")
+       .call(yAxis)
+       .attr("id", "y-axis");
 
     
   }
